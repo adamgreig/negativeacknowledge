8abb8cbe1d20a7075290e76e23f04ead7ee0d78c
Adam Greig <adam@adamgreig.com>
2013-09-22 03:31:45 +0100

    update settings and gitignore

--- fabfile.py
+++ fabfile.py
@@ -8,13 +8,7 @@ DEPLOY_PATH = env.deploy_path
 
 # Remote server configuration
 production = 'adam@paladin:22'
-dest_path = '/var/www'
-
-# Rackspace Cloud Files configuration settings
-env.cloudfiles_username = 'my_rackspace_username'
-env.cloudfiles_api_key = 'my_rackspace_api_key'
-env.cloudfiles_container = 'my_cloudfiles_container'
-
+dest_path = '~/negack/'
 
 def clean():
     if os.path.isdir(DEPLOY_PATH):
@@ -40,14 +34,6 @@ def reserve():
 
 def preview():
     local('pelican -s publishconf.py')
-
-def cf_upload():
-    rebuild()
-    local('cd {deploy_path} && '
-          'swift -v -A https://auth.api.rackspacecloud.com/v1.0 '
-          '-U {cloudfiles_username} '
-          '-K {cloudfiles_api_key} '
-          'upload -c {cloudfiles_container} .'.format(**env))
 
 @hosts(production)
 def publish():
--- pelicanconf.py
+++ pelicanconf.py
@@ -4,28 +4,45 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Adam Greig'
 SITENAME = u'Negative Acknowledge'
-SITEURL = ''
+SITEURL = 'http://negativeacknowledge.com'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Europe/London'
 
 DEFAULT_LANG = u'en'
+
+FILENAME_METADATA = r'(?P<date>\d{4}-\d{2}-\d{2})_(?P<slug>.*)'
+
+TYPOGRIFY = True
+
+THEME = 'themes/pelican-bootstrap3'
+BOOTSTRAP_THEME = 'slate'
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = False
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{slug}'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{slug}/index.html'
+
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS = (('adamgreig.com', 'https://adamgreig.com'),
+         ('CU Spaceflight', 'http://cusf.co.uk'),
+         ('UK High Altitude Society', 'http://ukhas.org.uk'),
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'https://twitter.com/adamgreig'),
+          ('github', 'https://github.com/adamgreig'),
+          ('flickr', 'http://www.flickr.com/photos/randomskk'),
+          )
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
