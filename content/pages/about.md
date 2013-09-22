289b8d4db7de0d48cb971e2f177631c33b3dfc85
Adam Greig <adam@adamgreig.com>
2013-09-22 12:06:20 +0100

    update about, config, pygments colours

--- content/pages/about.md
+++ content/pages/about.md
@@ -8,11 +8,18 @@ Summary: About this blog
 This is [Adam Greig](https://adamgreig.com)'s blog. Mostly posts about
 electronics, programming, signal processing and baking.
 
+Powered by [Pelican](http://docs.getpelican.com/en/3.2/), with the
+[Pelican-Bootstrap3](https://github.com/getpelican/pelican-themes/tree/master/pelican-bootstrap3)
+theme in [Journal](http://bootswatch.com/journal/) colours.
+
+Use the handy links on the side bar if you want to get in touch!
+
+
 ### History
 
 Used to be a Wordpress site ('08-'11).
 
-Moved over to a static generator (Pelican) when that became the thing to do.
+Moved over to a static generator when that became the thing to do.
 Most of the old content got imported, though!
 
 ![Screenshot of Wordpress version](|filename|/images/negativeacknowledge_wordpress.png)
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,46 +2,56 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+# Site Metadata
 AUTHOR = u'Adam Greig'
 SITENAME = u'Negative Acknowledge'
 SITEURL = 'http://negativeacknowledge.com'
-
 TIMEZONE = 'Europe/London'
-
 DEFAULT_LANG = u'en'
 
+# Path Settings
 FILENAME_METADATA = r'(?P<date>\d{4}-\d{2}-\d{2})_(?P<slug>.*)'
-
-THEME = 'themes/pelican-bootstrap3'
-BOOTSTRAP_THEME = 'slate'
-DISPLAY_PAGES_ON_MENU = True
-DISPLAY_CATEGORIES_ON_MENU = False
-
-DEFAULT_PAGINATION = 10
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-
 YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
 MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
 DAY_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/index.html'
-
 ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}'
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 
-# Blogroll
+# Content settings
+USE_FOLDER_AS_CATEGORY = False
+DEFAULT_CATEGORY = 'Uncategorised'
+ARTICLE_EXCLUDES = ['pages', '.ipynb_checkpoints']
+
+# Appearance Settings
+THEME = 'themes/pelican-bootstrap3'
+BOOTSTRAP_THEME = 'journal'
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = False
+DEFAULT_PAGINATION = 10
+
+# Plugins
+MARKUP = ('md', 'ipynb')
+PLUGIN_PATH = 'plugins'
+PLUGINS = ['ipythonnb']
+
+# Development settings, overriden by deployconf.py
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+RELATIVE_URLS = True
+
+
+# Web links
 LINKS = (('adamgreig.com', 'https://adamgreig.com'),
+         ('M0RND.net', 'http://m0rnd.net'),
+         ('randomskk.net', 'https://randomskk.net'),
          ('CU Spaceflight', 'http://cusf.co.uk'),
          ('UK High Altitude Society', 'http://ukhas.org.uk'),
+         ('HabHub', 'http://habhub.org'),
          )
 
-# Social widget
+# Social links
 SOCIAL = (('twitter', 'https://twitter.com/adamgreig'),
           ('github', 'https://github.com/adamgreig'),
           ('flickr', 'http://www.flickr.com/photos/randomskk'),
           )
-
-# Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
